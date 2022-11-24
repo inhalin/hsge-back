112b4a8030986d58227515510325b4f4719fd261
dlrgks88 <dlrgks88@naver.com>
2022-11-24 16:09:27 +0900

    feat: 엔티티 개발 #1
    유저 엔티티와 BaseEntity를 구현했습니다.

---
+++ src/main/java/hsge/hsgeback/entity/User.java
@@ -0,0 +1,34 @@
+package hsge.hsgeback.entity;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@NoArgsConstructor
+public class User extends BaseEntity{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true)
+    private String email;
+
+    @Column(unique = true)
+    private String nickname;
+
+    private String password;
+
+    private Double latitude;
+
+    private Double longtitude;
+
+    private String role;
+    
+    private String profilePath;
+
+}
