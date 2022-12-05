6d844395e19a3f196f1a1f56aaec55a599b435c9
Taemin Lee <dlrgks88@naver.com>
2022-12-05 19:07:56 +0900

    feat: UserRepository 유저 반경 찾기 기능 추가  #3

--- src/main/java/hsge/hsgeback/repository/UserRepository.java
+++ src/main/java/hsge/hsgeback/repository/UserRepository.java
@@ -3,6 +3,7 @@ package hsge.hsgeback.repository;
 import hsge.hsgeback.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
@@ -12,4 +13,5 @@ public interface UserRepository extends JpaRepository<User, Long> {
     boolean existsByEmail(String email);
 
     Optional<User> findByEmail(String email);
+    List<User> findByLatitudeBetweenAndLongtitudeBetween(Double startLatitude, Double endLatitude, Double startLongtitude, Double endLongtitude);
 }
