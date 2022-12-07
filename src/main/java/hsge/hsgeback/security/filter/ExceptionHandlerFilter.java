5baa2b0bfe95d1c60591b206831332dbceba2c3a
Seokju Hong <iamseokjuhong@gmail.com>
2022-12-07 16:12:44 +0900

    feat: 유효하지 않은 토큰 처리 #19
    카카오 Access Token 유효하지 않으면 예외 처리

---
+++ src/main/java/hsge/hsgeback/security/filter/ExceptionHandlerFilter.java
@@ -0,0 +1,43 @@
+package hsge.hsgeback.security.filter;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+import hsge.hsgeback.exception.TokenException;
+import lombok.Data;
+import org.springframework.http.MediaType;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Map;
+
+public class ExceptionHandlerFilter extends OncePerRequestFilter {
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        try {
+            filterChain.doFilter(request, response);
+        } catch (TokenException e) {
+            setErrorResponse(response, e.getMessage());
+        }
+    }
+
+    private void setErrorResponse(HttpServletResponse response, String errorCode) throws IOException {
+        response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+        response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+        Gson gson = new Gson();
+        JsonObject jsonObject = new JsonObject();
+        jsonObject.addProperty("message", errorCode);
+
+        String result = gson.toJson(jsonObject);
+        response.getWriter().write(result);
+    }
+
+    @Data
+    public static class ErrorResponse {
+        private final String message;
+    }
+}
