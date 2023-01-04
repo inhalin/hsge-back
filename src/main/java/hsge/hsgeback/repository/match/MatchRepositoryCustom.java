54d91451e05f908c59d7b2418b648dc2047037f1
inhalin <inhalin.kim@gmail.com>
2023-01-04 12:39:21 +0900

    fix: 강아지 카드 목록 이미 매칭한 강아지는 제외하도록 수정 #58

--- src/main/java/hsge/hsgeback/repository/match/MatchRepositoryCustom.java
+++ src/main/java/hsge/hsgeback/repository/match/MatchRepositoryCustom.java
@@ -1,5 +1,7 @@
 package hsge.hsgeback.repository.match;
 
+import java.util.List;
+
 public interface MatchRepositoryCustom {
 
     boolean existsByPetIdAndUserId(Long petId, Long LikerId);
@@ -7,4 +9,6 @@ public interface MatchRepositoryCustom {
     void deleteAllByPetIdAndUserId(Long petId, Long userId);
 
     void updateLikeValue(Long petId, Long userId, boolean likeValue);
+
+    List<Long> findAllMatchedPetIds(Long userId);
 }
