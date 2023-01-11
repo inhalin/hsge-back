74ba09eb359ceb5734a68220333b4ab3d2843962
Seokju Hong <iamseokjuhong@gmail.com>
2023-01-11 16:15:25 +0900

    chore: 애플리케이션 파일 관리 #69
    - DB DDL 없는 계정들로 변경
    - firebase 경로 클래스패스로 변경

--- src/main/java/hsge/hsgeback/config/FirebaseConfig.java
+++ src/main/java/hsge/hsgeback/config/FirebaseConfig.java
@@ -5,24 +5,27 @@ import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
 import com.google.firebase.messaging.FirebaseMessaging;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.Resource;
 
 import java.io.FileInputStream;
 import java.io.IOException;
 
+@Slf4j
 @Configuration
 @RequiredArgsConstructor
 public class FirebaseConfig {
 
     @Value("${fcm.service-account-path}")
-    private String serviceAccountPath;
+    private Resource serviceAccountPath;
 
     @Bean
     FirebaseMessaging firebaseMessaging() throws IOException {
         FileInputStream serviceAccount =
-                new FileInputStream(serviceAccountPath);
+                new FileInputStream(serviceAccountPath.getFile().getAbsolutePath());
 
         FirebaseOptions options = FirebaseOptions.builder()
                 .setCredentials(GoogleCredentials.fromStream(serviceAccount))
